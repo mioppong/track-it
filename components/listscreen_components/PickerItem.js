d47ee7958d245ea121f6e280437d0cf5e5feb881
oppong <michaeloppong123@gmail.com>
2021-01-18 23:22:35 -0500

    pick image works, need to push these changes so i know what im workin with so far

--- components/listscreen_components/PickerItem.js
+++ components/listscreen_components/PickerItem.js
@@ -1,23 +1,61 @@
-import React from "react";
-import { Modal, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Modal, StyleSheet, Text, View, Image } from "react-native";
 import colors from "../../config/colors";
 import AppButton from "../AppButton";
+import * as ImagePicker from "expo-image-picker";
 
 // THIS FUNCTION IS PASSED 2 ITEMS THEY CAN CHOOSE FROM, MENU
 export default function PickerItem(props) {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const {
+          status,
+        } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
   return (
     <Modal animationType="fade" transparent visible={props.visible}>
-      <View style={styles.container}>
-        <View style={styles.inisdeContainer}>
+      <View style={styles.cameraModalContainer}>
+        <View style={styles.inisdeCameraModalContainer}>
           <AppButton
             iconName="camera"
             iconSize={80}
             style={styles.buttonStyle}
           />
+          {image && (
+            <Image
+              source={{ uri: image }}
+              style={{ width: 200, height: 200 }}
+            />
+          )}
+
           <AppButton
             iconName="camera-burst"
             iconSize={80}
             style={styles.buttonStyle}
+            onPress={pickImage}
           />
 
           <AppButton
@@ -33,11 +71,11 @@ export default function PickerItem(props) {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  cameraModalContainer: {
     flex: 1,
     backgroundColor: "rgba(1,1,1,0.5);",
   },
-  inisdeContainer: {
+  inisdeCameraModalContainer: {
     alignItems: "center",
     justifyContent: "space-around",
     height: "50%",
